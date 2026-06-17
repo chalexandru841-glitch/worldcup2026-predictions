750e18e3e9e2095485366b257e534b0f393c22c8
chalexandru841-glitch <chalexandru841@gmail.com>
2026-06-17 15:37:02 +0300

    ✨ animation: add AnimatePresence for page transitions

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,7 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate, useLocation } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
 import Layout from "./components/layout/Layout";
+import PageTransition from "./components/common/PageTransition";
 import HomePage from "./pages/HomePage";
 import PredictionsPage from "./pages/PredictionsPage";
 import LeaderboardPage from "./pages/LeaderboardPage";
@@ -8,17 +10,21 @@ import ProfilePage from "./pages/ProfilePage";
 import LoginPage from "./pages/LoginPage";
 
 export default function App() {
+  const location = useLocation();
+
   return (
-    <Routes>
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/" element={<Layout />}>
-        <Route index element={<HomePage />} />
-        <Route path="predictions" element={<PredictionsPage />} />
-        <Route path="leaderboard" element={<LeaderboardPage />} />
-        <Route path="bracket" element={<BracketPage />} />
-        <Route path="profile" element={<ProfilePage />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Route>
-    </Routes>
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<PageTransition><HomePage /></PageTransition>} />
+          <Route path="predictions" element={<PageTransition><PredictionsPage /></PageTransition>} />
+          <Route path="leaderboard" element={<PageTransition><LeaderboardPage /></PageTransition>} />
+          <Route path="bracket" element={<PageTransition><BracketPage /></PageTransition>} />
+          <Route path="profile" element={<PageTransition><ProfilePage /></PageTransition>} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Route>
+      </Routes>
+    </AnimatePresence>
   );
 }
